4d010b4043f4207f62d53e082255c35dd2e350ad
Briden Dafoe <briden@vortechsolutions.com>
2025-02-24 16:04:57 -0700

    updated dep, test

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
-import adapter from "@sveltejs/adapter-auto";
-import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
+import { sveltePreprocess } from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: sveltePreprocess(),
 
 	kit: {
-		adapter: adapter(),
-
-		package: {
-			dir: "dist",
-			files: filepath => !/.stories.svelte|.test.ts/g.test(filepath),
-		},
+		adapter: adapter({ pages: "build", assets: "build", fallback: "index.html", precompress: true, strict: true }),
 	},
 };
 
