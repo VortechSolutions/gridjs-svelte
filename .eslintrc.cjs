69c4d81c8c73ff9ff83325b3d78271224c834cf7
Jared Forth <jaredforthdev@gmail.com>
2025-02-27 15:45:24 -0600

    chore: add release please (#3)
    Release-As: 2.1.0

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,26 +6,23 @@ module.exports = {
 	ignorePatterns: ["*.cjs"],
 	overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
 	settings: {
-		"svelte3/typescript": () => require("typescript")
+		"svelte3/typescript": () => require("typescript"),
 	},
 	parserOptions: {
 		sourceType: "module",
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
 	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
+		node: true,
 	},
 	rules: {
 		"@typescript-eslint/explicit-module-boundary-types": "off",
 		"@typescript-eslint/explicit-function-return-type": ["off"],
 		"@typescript-eslint/unbound-method": "off",
-		"@typescript-eslint/no-unused-vars": [
-			"warn",
-			{ argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
-		],
+		"@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
 		"import/prefer-default-export": "off",
-		"no-warning-comments": "off"
-	}
+		"no-warning-comments": "off",
+	},
 };
