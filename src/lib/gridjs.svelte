a85aa160579fbcfb70c81f48165b6aa8e8da5dab
Jared Forth <jaredforthdev@gmail.com>
2025-03-05 11:01:02 -0600

    fix: gridjs table render

--- src/lib/gridjs.svelte
+++ src/lib/gridjs.svelte
@@ -1,23 +1,30 @@
 <script lang="ts">
-	import { onMount, createEventDispatcher } from "svelte";
+	import { createEventDispatcher } from "svelte";
 	import { Grid } from "gridjs";
 
-	export let width = "100%";
-	export let height = "auto";
-	export let autoWidth = true;
-	export let fixedHeader = false;
-	export let resizable = false;
-	export let from = undefined;
-	export let language = undefined;
-	export let search = false;
-	export let sort = false;
-	export let pagination = false;
-	export let server = undefined;
-	export let columns = undefined;
-	export let data = undefined;
-	export let plugins = undefined;
-	export let style = {};
-	export let className = {};
+	const { 
+		width = "100%", 
+		height = "auto",
+		autoWidth = true,
+		fixedHeader = false,
+		resizable = false,
+		from,
+		language,
+		search = false,
+		sort = false,
+		pagination = false,
+		server,
+		columns,
+		data,
+		plugins,
+		style = {},
+		className = {},
+		cellClick,
+		rowClick,
+		beforeLoad,
+		load,
+		ready,
+	} = $props();
 
 	// https://github.com/grid-js/gridjs/blob/master/src/config.ts
 	export const instance = new Grid({
@@ -40,42 +47,40 @@
 	});
 
 	let node: Element;
+
 	const dispatch = createEventDispatcher();
 
 	// https://github.com/grid-js/gridjs/blob/master/src/view/table/events.ts
 	instance.on("cellClick", (...args) => dispatch("cellClick", { ...args }));
 	instance.on("rowClick", (...args) => dispatch("rowClick", { ...args }));
-
 	// https://github.com/grid-js/gridjs/blob/master/src/view/events.ts
 	instance.on("beforeLoad", () => dispatch("beforeLoad"));
 	instance.on("load", data => dispatch("load", { ...data }));
 	instance.on("ready", () => dispatch("ready"));
 
-	$: if (node) {
-		instance
-			.updateConfig({
-				from,
-				data,
-				columns,
-				server,
-				search,
-				sort,
-				pagination,
-				language,
-				width,
-				height,
-				autoWidth,
-				fixedHeader,
-				style,
-				className,
-				resizable,
-			})
-			.forceRender();
-	}
-
-	onMount(() => {
-		if (node) {
+	$effect(() => {
+		const grid = instance.updateConfig({
+			from,
+			data,
+			columns,
+			server,
+			search,
+			sort,
+			pagination,
+			language,
+			width,
+			height,
+			autoWidth,
+			fixedHeader,
+			style,
+			className,
+			resizable,
+		});
+		if (node && node.childNodes.length === 0) {
 			instance.render(node);
+		}
+		if (grid && node.childNodes.length > 0) {
+			grid.forceRender();
 		}
 	});
 </script>
